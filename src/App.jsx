3e58551fa9ef3712e5869c70d8877ad3e255f83a
Azhar <azharahmad1352@gmail.com>
2026-06-11 20:10:55 +0500

    Ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 
+import { Routes,Route } from 'react-router-dom'
+import Dashboard1 from './pages/Dashboard1.jsx'
+import Dashboard2 from './pages/Dashboard2.jsx'
 import Dashboard3 from './pages/Dashboard3.jsx'
 
 const App = () => {
   return (
     <div>
-      <Dashboard3 />
+      <Routes>
+        <Route path="/" element={<Dashboard1 />} />
+        <Route path="/dashboard2" element={<Dashboard2 />} />
+        <Route path="/dashboard3" element={<Dashboard3 />} />
+      </Routes>
     </div>
   )
 }
